30b1dea4b66b2060a138e6d623eb7ae2fa02596e
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-03-23 17:12:52 +0700

    Update Bonjour.java

--- Hello/Bonjour.java
+++ Hello/Bonjour.java
@@ -5,5 +5,7 @@ public class Bonjour {
         System.out.println("Hello I'm ANG OUSA");
         System.out.println("I'm Mouylang");
         System.out.println("I am Sreylen");
+        System.out.println("Hello");
+
     }
 }
